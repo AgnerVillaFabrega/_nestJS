6c0fa897d1c81df5b27732dc6d2b902e43867623
AgnerVillaFabrega <agnerfabrega@gmail.com>
2024-10-23 18:39:34 -0500

    DTOs and validation of information

--- 02-car-dealership/src/cars/cars.service.ts
+++ 02-car-dealership/src/cars/cars.service.ts
@@ -1,37 +1,40 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { Car } from './interfaces/car.interface';
+import { v4 as uuid } from 'uuid';
+import { CreateCarDto, UpdateCarDto} from './dto';
 
 @Injectable()
 export class CarsService {
-  private cars = [
+  private cars: Car [] = [
     {
-      id: 1,
+      id: uuid(),
       brand: 'Toyota',
       model: 'Corolla',
-      year: 2020,
+      year: '2020',
     },
     {
-      id: 2,
+      id: uuid(),
       brand: 'Honda',
       model: 'Civic',
-      year: 2022,
+      year: '2022',
     },
     {
-      id: 3,
+      id: uuid(),
       brand: 'Jeep',
       model: 'Cherokee',
-      year: 2019,
+      year: '2019',
     },
     {
-      id: 4,
+      id: uuid(),
       brand: 'Toyota',
       model: 'Highlander',
-      year: 2024,
+      year: '2024',
     },
     {
-      id: 5,
+      id: uuid(),
       brand: 'Toyota',
       model: 'Sienna',
-      year: 2000,
+      year: '2000',
     },
   ];
 
@@ -39,10 +42,47 @@ export class CarsService {
     return this.cars;
   }
 
-  findOneById(id: number) {
+  findOneById(id: string) {
     const car = this.cars.find(car => car.id === id);
     if(!car) throw new NotFoundException(`Car with id '${id}' not found`);
     
     return car;
   }
+
+  create(createCarDto: CreateCarDto) {
+    const newCar: Car = {
+      id:uuid(),
+      ...createCarDto
+      // model: createCarDto.model,
+      // brand: createCarDto.brand,
+      // year: createCarDto.year,
+    }
+    this.cars.push(newCar);
+    return newCar;
+  }
+
+  update(id: string, updateCarDto:UpdateCarDto){
+
+    let carDB  = this.findOneById(id);
+    if(updateCarDto.id && updateCarDto !== id)
+      throw new BadRequestException(`Car id '${id}'is not valid`);
+
+    this.cars = this.cars.map( car =>{
+      if (car.id === id){
+        carDB = { ...carDB, ...updateCarDto, id }
+        return carDB;
+      }
+      return car;
+    })
+
+    return carDB; //update car
+
+  }
+
+  delete(id: string) {
+    const car = this.findOneById(id);
+    this.cars = this.cars.filter(car => car.id !== id);
+    return `Car: '${car.id}' was deleted`; 
+  }
+
 }
---
+++ 02-car-dealership/src/cars/dto/create-car.dto.ts
@@ -0,0 +1,16 @@
+import { IsNumber, IsString, MinLength } from "class-validator";
+
+export class CreateCarDto{
+    @IsString()
+    readonly brand: string;
+
+    @IsString()
+    // @MinLength(3)
+    readonly model: string;
+
+    @IsString()
+    readonly year: string;
+
+}
+
+
