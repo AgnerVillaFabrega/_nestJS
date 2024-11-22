8cbb2855098f4f127cbff30ab2cdada153df5b5a
AgnerVillaFabrega <adejesusvilla@unicesar.edu.co>
2024-11-22 17:23:47 -0500

    section-10

---
+++ 04-teslo-shop/src/products/entities/product.entity.ts
@@ -0,0 +1,76 @@
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  title: string;
+
+  @Column('float', {
+    default: 0,
+  })
+  price: number;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  desciption: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  slug: string;
+
+  @Column('int', {
+    default: 0,
+  })
+  stock: number;
+
+  @Column('text', {
+    array: true,
+  })
+  size: string[];
+
+  @Column('text')
+  gender: string;
+
+  @Column('text',{
+    array:true,
+    default: []
+  })
+  tags:string[];
+
+  //images
+
+  @BeforeInsert()
+  // @BeforeUpdate()
+  checkSlug() {
+    
+    if (!this.slug) {
+      this.slug = this.title;
+    }
+    this.slug = this.slug
+      .toLocaleLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '');
+  }
+
+  @BeforeUpdate()
+  checkSlugUpdate(){
+    this.slug = this.slug
+      .toLocaleLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '');
+  }
+}
